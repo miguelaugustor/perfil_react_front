2959027b85b73c2c5de7502028a8b0d1ed1f7ac8
Miguel Augusto <migguelaugustolima@gmail.com>
2025-08-21 05:05:55 +0000

    primeiro_com

---
+++ my-app/app/Profile.js
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <div>
+      <img src="/minhaFotoPerfil.jpg" alt="foto miguel" width={150} />
+      <h2>Miguel Augusto</h2>
+      <p>Sou estudante de Sistemas para Internet. Gosto de aprender novas tecnologias.</p>
+    </div>
+  );
+}
---
+++ my-app/app/page.js
@@ -0,0 +1,10 @@
+import Profile from './Profile';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Site criado para a disciplina Programação Front-End</h1>
+      <Profile />
+    </div>
+  );
+}
